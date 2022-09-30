449f567e807db79f5376e3313ca7ada4338720b4
Mehdi Boghraei <mahdib619@gmail.com>
2022-09-30 10:41:33 +0330

    two-fer

---
+++ python/two-fer/two_fer.py
@@ -0,0 +1,9 @@
+"""Two-Fer expression"""
+
+def two_fer(name = 'you'):
+    """
+    :param name: str - the name in two-fer expression
+    :return: str - the two-fer expression for name
+    """
+
+    return f'One for {name}, one for me.'
